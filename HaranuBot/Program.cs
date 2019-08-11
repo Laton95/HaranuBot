ff1a61190006b4b5bdc4391ee58dce41ab2c23a0
Laton95 <laton95@gmail.com>
2019-08-11 18:56:31 +1000

    Add project files.

---
+++ HaranuBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HaranuBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
